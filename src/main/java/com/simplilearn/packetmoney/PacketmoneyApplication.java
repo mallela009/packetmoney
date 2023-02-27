449c710f80c20ef41cd38bd03a19b1cd790ecf82
mallela009 <mallelaarjuna009@gmail.com>
2023-02-27 14:32:09 +0530

    first commit

---
+++ src/main/java/com/simplilearn/packetmoney/PacketmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.packetmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacketmoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/packetmoney/PacketmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.packetmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacketmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
